ded942d5c9401baca7edbf052a96a7535fa38082
MinhDang98 <minhdangtn28@gmail.com>
2025-08-04 13:43:58 +0700

    Updated training phase

--- libs/agent.py
+++ libs/agent.py
@@ -1,155 +1,191 @@
-import gymnasium as gym
+import json
 import os
 import torch
 from stable_baselines3 import DQN
 from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnNoModelImprovement
 from .quantum_state_preparation import QuantumStatePreparation, TargetState
 
+DIR_NAME = "QSP_RL/"
+MODEL_NAME = "DQN_RL"
+
 class QuantumnAgent():
-    def __init__(self, target_state: TargetState, total_timesteps, eval_frequency, eval_episode, verbose):
-        # Set up basic properties
-        self.target_state = target_state
-        self.total_timesteps = total_timesteps
-        self.eval_frequency = eval_frequency
-        self.eval_episode = eval_episode
-        self.verbose = verbose
-        
-        self.log_dir = "./logs/DQN_Bell_State"
-        os.makedirs(self.log_dir, exist_ok=True)
-        
-        self.model_name = "DQN_Bell_State"
-        
-        # Determine the device to use
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        print(f"Using device: {self.device}")
-        
-        # Set up environment
-        self.initialize_environment()
-        
-        # Prepare the callback during training
-        self.set_up_callbacks()
-        
-        # Set up model
-        self.set_up_model()
-        
-        # Evaluate after training
-        self.train_model()
+	def __init__(self, 
+				 target_states_list: list[TargetState] = None, 
+				 total_timesteps: int = None, 
+				 eval_frequency: float = None, 
+				 eval_episode: float = None,
+				 training_mode: bool = False,
+				 verbose: int = 0):
+					
+		self.log_dir = "./logs/" + DIR_NAME
+		os.makedirs(self.log_dir, exist_ok=True)
+		
+		self.model_dir = "./model/" + DIR_NAME
+		os.makedirs(self.model_dir, exist_ok=True)
+  
+		self.config_path = self.model_dir + "env_config.json"
+  
+		if training_mode:
+			print("Agent initialize in traning mode.")
 
-        self.env.close()
-        self.evaluate_env.close()
-        
-    def initialize_environment(self):
-        # make_vec_env handles creation and wrapping for stable-baselines3 compatibility.
-        # env_kwargs passes arguments to your QuantumStatePreparation.__init__
-        self.env = make_vec_env(QuantumStatePreparation,
-                           n_envs=1,
-                           env_kwargs={'target_state': self.target_state})
-        
-        self.evaluate_env = QuantumStatePreparation(self.target_state)
-        
-    def set_up_callbacks(self):
-        eval_callback = EvalCallback(
-            self.env,
-            best_model_save_path=os.path.join(self.log_dir, "DQN_Bell_State_Model"),
-            log_path=self.log_dir,
-            eval_freq=self.eval_frequency,
-            n_eval_episodes=self.eval_episode,
-            deterministic=True,
-            render=False,
-        )
+			if target_states_list is None:
+				raise ExceptionType("Missing Target State for the model")
+			self.target_states_list = target_states_list
+   
+			# Set up basic properties
+			self.total_timesteps = total_timesteps
+			self.eval_frequency = eval_frequency
+			self.eval_episode = eval_episode
+			self.verbose = verbose
 
-        self.callbacks = [eval_callback]
-    
-    def set_up_model(self):
-        self.model = DQN(
-            "MlpPolicy",                # Multi-Layer Perceptron policy (standard feedforward neural network)
-            self.env,
-            learning_rate=1e-3,
-            buffer_size=100000,
-            learning_starts=100,       # Number of steps before learning begins (to fill replay buffer)
-            batch_size=32,
-            train_freq=(4, "step"),     # Train every 4 steps
-            target_update_interval=100,
-            exploration_fraction=0.3,
-            exploration_final_eps=0.02,
-            policy_kwargs=dict(net_arch=[128,128]), # Two hidden layers of 64 neurons each
-            verbose=self.verbose,
-            tensorboard_log=self.log_dir,
-            device=self.device
-        )
-        
-    def train_model(self):
-        print(f"Starting DQN training for {self.total_timesteps} timesteps...")
-        self.model.learn(
-            total_timesteps=self.total_timesteps,
-            callback=self.callbacks,
-            progress_bar=True
-        )
-        print("Training finished.")
+			# Determine the device to use
+			self.device = "cuda" if torch.cuda.is_available() else "cpu"
+			print(f"Using device: {self.device}")
+   
+			# Save env config
+			self.save_env_config(self.config_path)
+      
+			# Set up environment
+			self.initialize_environment()
+			
+			# Prepare the callback during training
+			self.set_up_callbacks()
+			
+			# Set up model
+			self.set_up_model()
+			
+			# Evaluate after training
+			self.train_model()
 
-        # Save the final model
-        self.model.save(os.path.join(self.log_dir, self.model_name))
-        print(f"Final model saved to {os.path.join(self.log_dir, 'DQN_Bell_State_Model')}")
+			self.env.close()
+			self.evaluate_env.close()
+		else:
+			print("Agent initialize in testing mode.")
+			config = self.load_env_config(self.config_path)
+			self.max_env_qubits = config.get('max_env_qubits')
+			self.max_env_gates = config.get('max_env_gates')
+	
+	def save_env_config(self, path):
+		self.max_env_qubits = max(ts.num_qubits for ts in self.target_states_list)
+		self.max_env_gates = max(ts.max_gates for ts in self.target_states_list)
+  
+		config_data = {
+			'max_env_qubits': self.max_env_qubits,
+			'max_env_gates': self.max_env_gates # Saving this too for consistency
+		}
+		with open(path, 'w') as f:
+			json.dump(config_data, f)
+		print(f"Saved environment config to {path}")
+  
+	def load_env_config(self, path):
+		"""Loads environment configuration from a JSON file."""
+		with open(path, 'r') as f:
+			config_data = json.load(f)
+		print(f"Loaded environment config from {path}")
+		return config_data
+	
+	def initialize_environment(self, target_states_list: list[TargetState] = None):
+		# Make_vec_env handles creation and wrapping for stable-baselines3 compatibility.
+		# env_kwargs passes arguments to your QuantumStatePreparation.__init__
+		self.env = make_vec_env(QuantumStatePreparation,
+						n_envs=1,
+						env_kwargs={"target_states_list": self.target_states_list,
+									"max_env_qubits": self.max_env_qubits,
+									"max_env_gates": self.max_env_gates})
+		
+		self.evaluate_env = QuantumStatePreparation(self.target_states_list, self.max_env_qubits, self.max_env_gates)
+   
+	def initialize_environment_for_infererence(self, target_state: TargetState):
+		# For inference, we just need 1 state
+		self.env = make_vec_env(QuantumStatePreparation,
+			n_envs=1,
+			env_kwargs={"target_states_list": [target_state],
+						"max_env_qubits": self.max_env_qubits,
+						"max_env_gates": self.max_env_gates})
+	
+		self.evaluate_env = QuantumStatePreparation([target_state], self.max_env_qubits, self.max_env_gates)
 
-    def eval_model(self):
-        print("\n--- Evaluating the Best Model ---")
-        best_model_path = os.path.join(self.log_dir, self.model_name + ".zip")
-        
-        if os.path.exists(best_model_path):
-            best_model = DQN.load(best_model_path, env=self.env)
-            
-            success_count = 0
-            total_eval_episode = 50
-            
-            for i in range(total_eval_episode):
-                obs, info = self.evaluate_env.reset()
-                done = False
-                steps = 0
+	def set_up_callbacks(self):
+		stop_train_callback = StopTrainingOnNoModelImprovement(
+			max_no_improvement_evals=50,
+			min_evals=5,
+			verbose=1
+		)
+		
+		eval_callback = EvalCallback(
+			self.env,
+			log_path=self.log_dir,
+			eval_freq=self.eval_frequency,
+			n_eval_episodes=self.eval_episode,
+			deterministic=True,
+			render=False,
+			callback_after_eval=stop_train_callback
+		)
 
-                while not done and steps < self.evaluate_env.max_gates + 2:
-                    action, state = best_model.predict(obs, deterministic=True)
-                    obs, reward, terminated, truncated, info_eval = self.evaluate_env.step(action)
-                    done = terminated or truncated
-                    steps += 1
-                    if terminated:
-                        success_count += 1
-                        break
-                    elif truncated:
-                        break
-                    
-            print(f"\n--- Best Model Evaluation Summary ---")
-            print(f"Successfully in {success_count}/{total_eval_episode} episodes ({success_count/total_eval_episode:.2%}).")
-        else:
-            print(f"No best model found at {best_model_path}")
+		self.callbacks = [eval_callback]
+	
+	def set_up_model(self):
+		self.model = DQN(
+			"MlpPolicy",
+			self.env,
+			learning_rate=1e-3,
+			buffer_size=1000000,
+			learning_starts=100,
+			batch_size=128,
+			train_freq=(4, "step"),
+			target_update_interval=100,
+			exploration_fraction=0.3,
+			exploration_final_eps=0.02,
+			policy_kwargs=dict(net_arch=[256, 256]),
+			verbose=self.verbose,
+			tensorboard_log=self.log_dir,
+			device=self.device
+		)
+		
+	def train_model(self):
+		print(f"Starting DQN training for {self.total_timesteps} timesteps...")
+		self.model.learn(
+			total_timesteps=self.total_timesteps,
+			callback=self.callbacks,
+			progress_bar=True
+		)
+		print("Training finished.")
 
-    def build_circuit(self):
-        best_model_path = os.path.join(self.log_dir, self.model_name + ".zip")
-        if os.path.exists(best_model_path):
-            best_model = DQN.load(best_model_path, env=self.env)
-            
-            success_count = 0
-            total_eval_episode = 50
-            
-            for i in range(total_eval_episode):
-                obs, info = self.evaluate_env.reset()
-                done = False
-                steps = 0
+		# Save the final model
+		self.model.save(self.model_dir + MODEL_NAME + ".zip")
+		print(f"Final model saved to {self.model_dir}")
 
-                while not done and steps < self.evaluate_env.max_gates:
-                    action, state = best_model.predict(obs, deterministic=True)
-                    obs, reward, terminated, truncated, info_eval = self.evaluate_env.step(action)
-                    done = terminated or truncated
-                    steps += 1
-                    # We found the circuit
-                    if terminated:
-                        print(f"Circuit successfully built in {steps} steps with fidelity: {info_eval['fidelity']:.4f}")
-                        return self.evaluate_env.qc
-                    elif truncated:
-                        print(f"Circuit building truncated after {steps} steps (max gates reached). Fidelity: {info_eval['fidelity']:.4f}")
-                        return None
-                    
-            print(f"Circuit building finished without reaching target fidelity within {self.evaluate_env.max_gates} steps. Fidelity: {info_eval['fidelity']:.4f}")
-            return None
-        else:
-            print(f"No best model found at {best_model_path}")
+	def build_circuit(self, 
+					  target_state: TargetState):
+		best_model_path = os.path.join(self.model_dir, MODEL_NAME + ".zip")
+		
+  
+		# Set up environment
+		self.initialize_environment_for_infererence(target_state)
+   
+		if os.path.exists(best_model_path):
+			best_model = DQN.load(best_model_path, env=self.env)
+			total_eval_episode = 50
+			for i in range(total_eval_episode):
+				obs, info = self.evaluate_env.reset(target_state_object=target_state)
+				done = False
+				steps = 0
+
+				while not done and steps < self.evaluate_env.max_gates:
+					action, state = best_model.predict(obs, deterministic=True)
+					obs, reward, terminated, truncated, info_eval = self.evaluate_env.step(action)
+					done = terminated or truncated
+					steps += 1
+					# We found the circuit
+					if terminated:
+						print(f"Circuit successfully built in {steps} steps with fidelity: {info_eval['fidelity']:.4f}")
+						return self.evaluate_env.qc
+					elif truncated:
+						print(f"Circuit building truncated after {steps} steps (max gates reached). Fidelity: {info_eval['fidelity']:.4f}")
+						return None
+					
+			print(f"Circuit building finished without reaching target fidelity within {self.evaluate_env.max_gates} steps. Fidelity: {info_eval['fidelity']:.4f}")
+			return None
+		else:
+			print(f"No best model found at {best_model_path}")
--- libs/quantum_state_preparation.py
+++ libs/quantum_state_preparation.py
@@ -1,5 +1,6 @@
 import gymnasium as gym
 import numpy as np
+import random
 from enum import Enum
 from gymnasium import spaces
 from qiskit import QuantumCircuit
@@ -12,18 +13,61 @@ class TargetStateName(Enum):
 	BELL_STATE_PSI = "BELL_STATE_PSI"
 	COMPUTATIONAL_BASIS_STATE = "COMPUTATIONAL_BASIS_STATE"
 	UNIFORM_SUPERPOSITION = "UNIFORM_SUPERPOSITION"
-	
+
+class GeneralTargetState:
+	def __init__(self,
+				 target_state_name: TargetStateName,
+				 num_qubits: int):
+		self.state_name = target_state_name.value
+		self.num_qubits = num_qubits
+		self.max_gates =  2 * num_qubits
+  
+		if target_state_name == TargetStateName.GHZ_STATE:
+			self.target_vector = self.generate_ghz_target_vector()
+		elif target_state_name == TargetStateName.UNIFORM_SUPERPOSITION:
+			self.target_vector = self.generate_uniform_superposition_target_vector()
+		else:
+			print(f"Unexpected Target State: {target_state_name}")
+   
+	def generate_ghz_target_vector(self):
+		vector_size = 2 ** self.num_qubits
+		ghz_vector = np.zeros(vector_size, dtype=complex)
+		amplitude = 1 / np.sqrt(2)
+		
+		# Set the first element (|0...0>) and the last element (|1...1>)
+		ghz_vector[0] = amplitude
+		ghz_vector[vector_size - 1] = amplitude
+		
+		return ghz_vector
+
+	def generate_uniform_superposition_target_vector(self):
+		vector_size = 2 ** self.num_qubits
+		uniform_amplitude = 1 / np.sqrt(vector_size)
+		return np.full(vector_size, uniform_amplitude, dtype=complex)
+
+	def to_string(self):
+		return self.state_name
+
 class TargetState:
 	def __init__(self, 
-			  	 targetStateName: TargetStateName):
-		if targetStateName == TargetStateName.BELL_STATE:
+			  	 target_state_name: TargetStateName = None,
+	   			 target_vector: np.ndarray = None,
+				 num_qubits: int = None,
+				 max_gates: int = None):
+		self.state_name = target_state_name.value
+  
+		if target_state_name == TargetStateName.BELL_STATE:
 			# Bell State (|Φ+>)
-			# A 2-qubit entangled state
+			# For example, Bell state: |Phi+> = (1/sqrt(2)) * (|00> + |11>)
+			# Amplitude P = a^2 => (1/sqrt(2))^2 would be 1/2 which is 50%
+			# For 2 qubits, the computational basis states are |00>, |01>, |10>, |11>
+			# The statevector is a 4-element complex array: [amplitude_00, amplitude_01, amplitude_10, amplitude_11]
+			# So, for |Phi+>, it's [1/sqrt(2), 0, 0, 1/sqrt(2)]
 			bell_state_phi_plus_vector = np.array([1/np.sqrt(2), 0, 0, 1/np.sqrt(2)], dtype=complex)
 			self.num_qubits = 2
 			self.max_gates = 5
 			self.target_vector = bell_state_phi_plus_vector
-		elif targetStateName == TargetStateName.GHZ_STATE:
+		elif target_state_name == TargetStateName.GHZ_STATE:
 			# GHZ State for 3 Qubits (|GHZ>)
 			# A 3-qubit entangled state: (1/sqrt(2)) * (|000> + |111>)
 			# The vector will have 2^3 = 8 elements
@@ -31,81 +75,115 @@ class TargetState:
 			self.num_qubits = 3
 			self.max_gates = 8
 			self.target_vector = ghz_state_3_qubits_vector
-		elif targetStateName == TargetStateName.BELL_STATE_PSI:
+		elif target_state_name == TargetStateName.BELL_STATE_PSI:
 	  		# Another Bell State (|Ψ+>)
 			# (1/sqrt(2)) * (|01> + |10>)
 			bell_state_psi_plus_vector = np.array([0, 1/np.sqrt(2), 1/np.sqrt(2), 0], dtype=complex)
 			self.num_qubits = 2
 			self.max_gates = 5
 			self.target_vector = bell_state_psi_plus_vector
-		elif targetStateName == TargetStateName.COMPUTATIONAL_BASIS_STATE:
+		elif target_state_name == TargetStateName.COMPUTATIONAL_BASIS_STATE:
 	  		# Computational Basis State (|10>) for 2 Qubits
 			# Represents the state where the first qubit is 1 and the second is 0
 			computational_basis_10_vector = np.array([0, 0, 1, 0], dtype=complex)
 			self.num_qubits = 2
 			self.max_gates = 3
 			self.target_vector = computational_basis_10_vector
-		elif targetStateName == TargetStateName.UNIFORM_SUPERPOSITION:
+		elif target_state_name == TargetStateName.UNIFORM_SUPERPOSITION:
 			# Uniform Superposition for 2 Qubits
 			# (1/2) * (|00> + |01> + |10> + |11>)
 			uniform_superposition_2_qubits_vector = np.array([0.5, 0.5, 0.5, 0.5], dtype=complex)
 			self.num_qubits = 2
 			self.max_gates = 6
 			self.target_vector = uniform_superposition_2_qubits_vector
+		elif target_vector is not None and num_qubits is not None and max_gates is not None:
+			# This path allows direct instantiation with custom parameters
+			self.target_vector = target_vector
+			self.num_qubits = num_qubits
+			self.max_gates = max_gates
 		else:
-			print(f"Unexpected Target State: {targetStateName}")
+			print(f"Unexpected Target State: {target_state_name}")
+
+	def to_string(self):
+		return self.state_name
 		
 class QuantumStatePreparation(gym.Env):
 	metadata = { "render_modes": ["human"], "render_fps": 30}
 	
 	def __init__(self,
-				 target_state: TargetState):
+				 target_states_list: list[TargetState],
+	 			 max_env_qubits: int,
+				 max_env_gates: int):
 		super().__init__()
 
 		# General parameters
-		self.num_qubits = target_state.num_qubits
-		self.max_gates = target_state.max_gates
+		self.target_states_list = target_states_list
 		self.current_gates_count = 0
 		self.current_fidelity = -1
 		self.previous_fidelity = 0
 		
-		# Target state
-		# For example, Bell state: |Phi+> = (1/sqrt(2)) * (|00> + |11>)
-		# Amplitude P = a^2 => (1/sqrt(2))^2 would be 1/2 which is 50%
-		# For 2 qubits, the computational basis states are |00>, |01>, |10>, |11>
-		# The statevector is a 4-element complex array: [amplitude_00, amplitude_01, amplitude_10, amplitude_11]
-		# So, for |Phi+>, it's [1/sqrt(2), 0, 0, 1/sqrt(2)]
-		target_vector = target_state.target_vector
-		self.state_vector = Statevector(target_vector)
-		
-		# Action space
-		self.action_set = self.generate_action_state(self.num_qubits)
-		self.action_space = spaces.Discrete(len(self.action_set))
+		if not self.target_states_list:
+			raise ValueError("target_states_list cannot be empty.")
 
-		# Observer
-		# We represent the quantum state as the full statevector (complex numbers).
-		# Since RL models typically work with real numbers, we flatten the real and imaginary parts.
-		# For N qubits, the statevector has 2^N complex components.
-		# So, observation dimension = 2 * (2^N) real numbers.
-		obs_dim = 2 * (2 ** self.num_qubits)
-		self.observation_space = spaces.Box(low=-1.0, high=1.0, shape=(obs_dim,))
+		# --- Determine the maximum number of qubits and max gates for the environment's fixed spaces ---
+		self.max_env_qubits = max_env_qubits
+		self.max_env_gates = max_env_gates
 		
-  		# Initialize the quantum circuit and state to their default starting values.
+		# Initialize the quantum circuit and state to their default starting values.
 		self.reset()
 
+		# Action space
+		self.action_set = self.generate_action_state(self.max_env_qubits)
+		self.action_space = spaces.Discrete(len(self.action_set))
+
+		# Observer includes both current state AND target state
+		# We represent the quantum state as the full statevector (complex numbers).
+		# Since RL models typically work with real numbers, we flatten the real and imaginary parts.
+		# For N qubits, current_state is 2 * 2 ^ N, target_state is 2 * 2 ^ N
+		# So, observation dimension = 2 * (2 ^ N) real numbers.
+		obs_dim = 4 * (2 ** self.max_env_qubits)
+		# When we define gymnasium.spaces.Box(low=-1.0, high=1.0, shape=(obs_dim,), dtype=np.float32), 
+		# this shape is immutable for the lifetime of the environment instance. 
+		# Stable Baselines3 initializes internal buffers and network structures based on this fixed shape.
+		self.observation_space = spaces.Box(low=-1.0, high=1.0, shape=(obs_dim,))
+
+	def set_up_target_state(self, target_state_object: TargetState = None):
+		self.target_state_object: TargetState = None
+		if target_state_object is not None:
+			self.target_state_object = target_state_object
+		else:
+			self.target_state_object = random.choice(self.target_states_list)
+		self.state_vector = Statevector(self.target_state_object.target_vector)
+		self.num_qubits = self.target_state_object.num_qubits
+		self.max_gates = self.target_state_object.max_gates
+  
 	def _get_obs(self):
-		current_state_vector = self.current_circuit_state.data
-		# Concatenate the real parts and imaginary parts into a single 1D numpy array.
-		# For exmaple, for 2 qubits, it will an 8-dimensional vector with 4 amplitude. 
-		# So, we can understand it as a+ib. Hence, it 4 for real part and 4 for imaginary part
-		return np.array(np.concatenate((current_state_vector.real, current_state_vector.imag)), dtype=np.float32)
+		# Get the data for the current and target statevectors
+		current_sv_data = self.current_circuit_state.data
+		target_sv_data = self.state_vector.data
+
+		# Pad current_circuit_state and target_vector if their num_qubits is less than max_env_qubits
+		# The dimension of the statevector is 2^N.
+		padding_dim = 2**self.max_env_qubits - 2**self.num_qubits
+		
+		# Create zero arrays for padding
+		zero_pad = np.zeros(padding_dim, dtype=complex)
+
+		# Pad the actual state vectors if necessary
+		padded_current_sv_data = np.concatenate((current_sv_data, zero_pad)) if padding_dim > 0 else current_sv_data
+		padded_target_sv_data = np.concatenate((target_sv_data, zero_pad)) if padding_dim > 0 else target_sv_data
+  
+		# Ensure all parts are float32
+		observation = np.concatenate((padded_current_sv_data.real, padded_current_sv_data.imag,
+									  padded_target_sv_data.real, padded_target_sv_data.imag)).astype(np.float32)
+		return observation
 
 	def obs_to_string(self, obs):
-		half = int(len(obs) / 2)
-		print(f"\tNew Observation:")
-		# This is the real part
-		for i in range(half):
-			print(f"\t\tReal value: {obs[i]} - Imaginary value: {obs[i + half]}")
+		quarter = int(len(obs) / 4) # Now 4 parts: current_real, current_imag, target_real, target_imag
+		print(f"\tNew Observation (Current | Target):")
+		for i in range(quarter):
+			print(f"\t\tCurrent Real: {obs[i]:.4f} - Current Imag: {obs[i + quarter]:.4f}")
+			print(f"\t\tTarget Real: {obs[i + 2*quarter]:.4f} - Target Imag: {obs[i + 3*quarter]:.4f}")
 
 	def _get_info(self):
 		# Calculate fidelity: Measures how close the current state is to the target state.\
@@ -134,12 +212,15 @@ class QuantumStatePreparation(gym.Env):
 						# { "gate": CXGate(), "qubits": [0, 1]},
 						action_space.append({"gate": g, "qubits": [i, j]})
 		return action_space
-    
-	def reset(self, seed=None, options=None):
+	
+	def reset(self, seed=None, target_state_object: TargetState = None):
 		super().reset(seed=seed)
+  
+		self.set_up_target_state(target_state_object)
+		
 		# Initialize a fresh quantum circuit with the specified number of qubits.
 		self.qc = QuantumCircuit(self.num_qubits)
-		
+
   		# Statevector.from_int(0, dim) creates a statevector where only the |0...0> component is 1.
 		self.current_circuit_state = Statevector.from_int(0, 2**self.num_qubits)
 
@@ -155,7 +236,20 @@ class QuantumStatePreparation(gym.Env):
 		gate_info = self.action_set[action]
 		gate = gate_info["gate"]
 		qubits = gate_info ["qubits"]
-		
+          
+        # If the chosen action targets a qubit index beyond the current target's num_qubits,
+		# or involves qubits not relevant to the current state,
+		# penalize heavily and truncate, or ignore (penalize is better for RL).
+		# This is a simple check; more complex masking might be needed for very large action spaces.
+		if any(q >= self.num_qubits for q in qubits):
+			# Penalize for applying gate to non-existent or inactive qubit
+			reward = -10.0 # Large penalty
+			terminated = False
+			truncated = True
+			observation = self._get_obs() # Still return observation
+			info = self._get_info()
+			return observation, reward, terminated, truncated, info
+
 		# Append the gate to the actual circuit object (self.qc)
 		self.qc.append(gate, qubits)
 		
